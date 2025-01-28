8bb7dec5a8712fd001d85f7299587f59fb20274e
regilozano <regina.loz.hdz@gmail.com>
2025-01-27 20:03:50 -0600

    Create Tarea1.py

---
+++ Semana 1/Tarea1.py
@@ -0,0 +1,30 @@
+#Pregunta 1
+print('c')
+
+#Pregunta 2 
+print('conjunto de instrucciones escritas en un lenguaje de porgramación con el fin de ejecutar una tarea con mayor eficiencia')
+
+#Pregubnta 3
+print('el intérprete trabaja directamente con python, ejecutando el código fuente. Mientras que el compilador traduce el código fuente a código máquina')
+
+#Pregunta 4 
+print('ninguna de las opciones contiene código máquina')
+
+#Pregunta 5
+print('la función "primt" esta mal escrita, debería ser escrita "print"')
+
+#Pregunta 6 
+print('b')
+
+#Pregunta 7 
+print('b')
+
+#Pregunta 8
+print('Unidad central de procesamiento: Parte de la computadora que hace posible su funcionamiento, pregunta qué hacer después y procesa las señales, etc. Se le conoce como el cerebro de la computadora, ya que procesa y ejecuta todo. ')
+print('Memoria Principal: Es donde se guarda la información que se necesita rápidamente, pero al apagar la computadora esta información se pierde. Un ejemplo es la capacidad de memorizar para un examen, poder contestar el examen correctamente, pero un día después ya no recuerdas la información, porque solo la memorizaste no la aprendiste.')
+print('Memoria secundaria: También guarda información, pero es más lenta y almacena esta información hasta cuando esta apagada la computadora. Un ejemplo es igual la memoria, pero la que se utiliza para recordar nombres, edades, operaciones básicas, tablas de multiplicar, etc.')
+print('Dispositivos de entrada: Son los dispositivos que permiten que entre información a la computadora. Un ejemplo sería nuestra visión, nos permite obtener toda la información de nuestro alrededor. ')
+print('Dispositivos de salida: Son los dispositivos que muestran la información que la computadora ya proceso, un ejemplo es nuestra capacidad de hablar, ya que nos permite expresar los pensamientos e información que tenemos en nuestro cerebro. ')
+
+#Pregunta 9
+print('estos errores representan faltas gramaticales, y se resuelven volviendo a escribir de forma correcta el comando. ')
